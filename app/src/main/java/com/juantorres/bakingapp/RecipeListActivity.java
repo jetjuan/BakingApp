abbe364be34c8b13a4682fa38f9d436255379fab
Juan Torres <juanetf@gmail.com>
2017-12-13 11:50:08 -0400

    Removed FABs

--- app/src/main/java/com/juantorres/bakingapp/RecipeListActivity.java
+++ app/src/main/java/com/juantorres/bakingapp/RecipeListActivity.java
@@ -61,15 +61,6 @@ public class RecipeListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         View recyclerView = findViewById(R.id.recipe_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
